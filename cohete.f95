c201be78027f13025ea5094da56fb3823ac4b603
MrIsCM <61559771+MrIsCM@users.noreply.github.com>
2022-05-12 18:59:41 +0200

    Create cohete.f95

---
+++ cohete.f95
@@ -0,0 +1,22 @@
+!=======================================
+! 		Ismael Charpentier
+!
+! 			12/05/2022
+!
+! 	  Algoritmo de Runge-Kutta
+! 	Problema de los tres cuerpos
+!
+!=======================================
+program cohete
+	implicit none
+
+	real :: h 		! Parametro de discretizacion 
+	integer :: t 
+
+	integer, parameter :: t_max = 1000
+
+	do t = 0, t_max 
+		
+	end do
+	
+end program cohete
