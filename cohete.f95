4457fd121030bca1494723ff57885a625a8ee9e0
MrIsCM <61559771+MrIsCM@users.noreply.github.com>
2022-05-13 00:52:29 +0200

    commit

--- cohete.f95
+++ cohete.f95
@@ -14,7 +14,6 @@ program cohete
 	integer :: t 	! Parametro de iteración temporal
 
 	integer, parameter :: t_max = 1000
-	real, parameter :: m = 20000 		! Masa del cohete [Kg]
 	
 	! Datos del problema
 	double precision :: Mt, Ml, m, G, dtl, w, Rt, Rl  
@@ -35,12 +34,21 @@ program cohete
 	Rt = 6.378160E6		! Radio Tierra [m] 
 	Rl = 1.7374E6		! Radio Luna [m] 
 
+	m = 10000 			! Masa del cohete [Kg]
+
 	!-------------------------
 	! 	Factores de escala
 	!-------------------------
 	fr = 1/dtl 
 	fpr = 1/(m*dtl)
 	fpphi = 1/(m*dtl**2)
+
+	!----------------------
+	!	Parametro h: 
+	!----------------------
+	! En primer lugar lo escojo "arbitrariamente" 
+	h = 60
+	! En segundo lugar, se utiliza el reajuste de h descrito en el guión
 
 
 
@@ -50,9 +58,73 @@ program cohete
 	! Bucle temporal
 	do t = 0, t_max 	
 
-	
 	end do
 
 	close(10)
 	
 end program cohete
+
+subroutine alg_RK(G, Mt, Ml, dtl, w, Rt, Rl, m, x_pos, y_pos)
+	double precision, intent(in) :: G, Mt, Ml, dtl, w, Rt, Rl
+	double precision, intent(inout) ::  x_pos, y_pos
+
+
+	
+end subroutine alg_RK
+
+!----------------------
+! 	(1) r/dt = ...
+!----------------------
+subroutine r_dt(pr, result)
+	real, intent(in) :: pr 
+	real, intent(out) :: result
+
+	result = pr 
+	
+end subroutine r_dt
+
+!----------------------
+! 	(2) phi/dt = ...
+!----------------------
+subroutine phi_dt(pphi, result)
+	real, intent(in) :: pphi
+	real, intent(out) :: result
+
+	result = pphi 
+	
+end subroutine phi_dt
+
+
+!----------------------
+! 	(3) dp_r/dt = ...
+!----------------------
+subroutine dp_dt(p_phi, delta, mu, r, r_p, phi, w, t, result)
+	real, intent(in) :: p_phi, delta, mu, r, r_p, phi, w, t
+	real, intent(out) :: result
+
+	result = p_phi**2/r**3 - delta*(1/r**2 + mu/r_p**3 *(r - cos(phi-w*t)))
+	
+end subroutine dp_dt
+
+
+!----------------------
+! 	(4) dp_phi/dt = ...
+!----------------------
+subroutine dpo_dt(delta, mu, r, r_p, phi, w, result)
+	real, intent(in) :: delta, mu, r, r_p, phi, w
+	real, intent(out) :: result
+
+	result = - delta*mu*r*sin(phi-w*t)/r_p**3 
+	
+end subroutine dpo_dt
+
+
+!---------------------------------------------------------------
+!	Función genérica que permite ampliar el algoritmo
+!	 para EDOs de cualquier tipo, definiendo f en este apartado
+!---------------------------------------------------------------
+! subroutine f(x, y, result)
+! 	real, intent(in) :: x, y
+! 	real, intent(out) :: result
+
+! end subroutine f
